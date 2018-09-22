5065b98a070f96590c91400047fd33be0a787954
sunfei <feisun@westwin.com>
2018-09-22 23:49:30 +0800

    add tracking.php

---
+++ atm/tracking/tracking.php
@@ -0,0 +1,30 @@
+<?php
+
+$DELI = '____';
+
+function _log($content, $theDeli) {
+	$dt = new DateTime("now", new DateTimeZone('Asia/Shanghai'));
+	$now = $dt->format('Y-m-d H:i:s');
+	$fnow = $dt->format('Ymd');
+	$filePath = '/var/www/html/logs/diabetes.'.$fnow.'.log';
+echo $filePath;
+	$fp = fopen($filePath, 'a');
+	$line = $now.$theDeli.$content."\n";
+	fwrite($fp, $line);
+	fclose($fp);
+}
+
+try {
+	$reqJson = file_get_contents('php://input');
+	$req = json_decode($reqJson, true);
+	$ua = $req['ua'];
+	$url = $req['url'];
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$content = $ua.$DELI.$url.$DELI.$ip;
+	_log($content, $DELI);
+	echo 'OK';
+} catch (Exception $e) {
+	echo 'ERROR';
+}
+
+?>
