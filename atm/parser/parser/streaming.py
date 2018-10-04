daf8c67a1dcf438d7d72200b9cc5cd4a46219ca1
sunfei <feisun@westwin.com>
2018-10-04 22:52:18 +0800

    update

--- atm/parser/parser/streaming.py
+++ atm/parser/parser/streaming.py
@@ -4,6 +4,7 @@
 import time
 import os
 import re
+import sys
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime
@@ -14,7 +15,7 @@ from url_helper import UrlHelper
 from parse_helper import ParseHelper
 
 MONGO_HOST = "172.16.40.128:27017,172.16.40.140:27017,172.16.40.141:27017"
-MONGO_HOST = "127.0.0.1:27017"
+#MONGO_HOST = "127.0.0.1:27017"
 MONGO_DATABASE_NAME = "ZDBWordPress"
 MONGO_KEY_COLLECTION = "key"
 MONGO_PAGE_COLLECTION = "page"
@@ -31,46 +32,43 @@ DELETE_URL = 'http://www.infosoap.com/wp-content/plugins/post-api/delete_post.ph
 DELI = '____'
 PERMALINKS_URL = 'http://www.infosoap.com/wp-content/plugins/post-tester/get_all_permalinks.php'
 
-def uploadPage(source):
+def uploadKeyPage():
     try:
         total = 0
         while True:
-            if source == 'page':
-                docList = pageCollection.nextPage(20)
-            else:
-                docList = keyCollection.nextPage(20)
+            docList = keyCollection.nextPage(20)
             if docList == None or len(docList) == 0:
                 break
 
             for doc in docList:
-                if doc['state'] != 'PARSED':
+                if doc['state'] != 'GENERATED':
                     continue
 
                 # search wp by title
-                req = {
-                    'title': doc['title']
-                }
-                errorCode, rsp = HttpHelper.post(QUERY_URL, req)
-                if errorCode != 'OK':
-                    raise Exception('query error, url=' + doc['url'])
-                if rsp['errorCode'] == 'ERROR':
-                    doc['state'] = 'DUPED'
-                    pageCollection.updateOne(doc)
-                    continue
+#                 req = {
+#                     'title': doc['finalTitle']
+#                 }
+#                 errorCode, rsp = HttpHelper.post(QUERY_URL, req)
+#                 if errorCode != 'OK':
+#                     raise Exception('query error, url=' + doc['url'])
+#                 if rsp['errorCode'] == 'ERROR':
+#                     doc['state'] = 'DUPED'
+#                     pageCollection.updateOne(doc)
+#                     continue
                 
                 # upload
-                postTitle = doc['pageTitle']
-                if postTitle == None:
-                    postTitle = doc['title']
-                postExcerpt = doc['pageDescription']
-                if postExcerpt == None:
-                    postExcerpt = doc['description']
+                postTitle = doc['finalTitle']
+                postExcerpt = doc['finalDescription']
+                postContent = doc['finalContent']
+                if postTitle == None or postExcerpt == None or postContent == None:
+                    raise Exception('invalid post, key=' + doc['title'])                    
+                
                 req = {
                     'ID': 0,
                     'author': 1,
                     'title': postTitle,
                     'excerpt': postExcerpt,
-                    'content': doc['content'],
+                    'content': postContent,
                     'categories': [1]
                 }
                 errorCode, rsp = HttpHelper.post(INSERT_URL, req)
@@ -171,4 +169,15 @@ def eliminatePost():
     
                                     
 if __name__=="__main__":
-    uploadPage('key')
+
+    cmd = 'upload'
+    if len(sys.argv) == 2:
+        cmd = sys.argv[1]
+    else:
+        cmd = 'upload'
+    
+    if cmd == 'upload':
+        uploadKeyPage()
+    else:
+        print ('unknwo cmd={0}'.format(cmd))
+        
