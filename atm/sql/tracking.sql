550626e0ea8e0b14837883155f7104c69aacdb13
sunfei <feisun@westwin.com>
2018-10-11 15:30:37 +0800

    update

--- atm/sql/tracking.sql
+++ atm/sql/tracking.sql
@@ -2,7 +2,7 @@ CREATE TABLE `tracking` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(256) NOT NULL,
   `ua` varchar(512) NOT NULL,
-  `uip` varchar(32) NOT NULL,
+  `uip` varchar(64) NOT NULL,
   `referer` varchar(256) NULL,
   `createTime` datetime NOT NULL,
   PRIMARY KEY (`id`)
