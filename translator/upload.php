a4bb47e84bcdc069e8e7144dc58464b98bbf265e
feisun <feisun@westwin.com>
2019-05-10 13:28:20 +0800

    update

--- translator/upload.php
+++ translator/upload.php
@@ -4,32 +4,37 @@ Get Post.
 */
 
 # live
-# curl -XPOST -H"Content-Type:application/json" https://translate.tlvstream.com/upload.php -d"{\"title\": \"the title\", \"content\": \"the content\"}"
 
 function _upload_post($req) {
 	try {
 		// load template
 		$html = file_get_contents("template.html");
-		str_replace("{title}", $req->title, $html);
-		str_replace("{content}", $req->content, $html);
+		$html = str_replace("{title}", $req->title, $html);
+		$html = str_replace("{content}", $req->content, $html);
 		
 		$uid = uniqid();
 		$len = strlen($uid);
 		$prefix = substr($uid, $len - 1, 1);
-		$filePath = "./".$prefix."/".$uid.".html";
-		$fileUrl = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST'].str_replace("upload.php", "", $_SERVER['REQUEST_URI'])."/".$prefix."/".$uid.".html";
+
+		$dirPath = __DIR__."/".$prefix;
+		if (!file_exists($dirPath)) {
+			mkdir($dirPath, 0755, true);
+		}
+
+		$filePath = $dirPath.'/'.$uid.".html";
+		$fileUrl = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST'].str_replace("upload.php", "", $_SERVER['REQUEST_URI']).$prefix."/".$uid.".html";
 		file_put_contents($filePath, $html);
 		
 		return array(
 			"errorCode" => "OK",
 			"errorMessage" => "",
-			"url" = $fileUrl,
+			"url" => $fileUrl,
 		);
 	} catch (Exception $e) {
 		return array(
 			"errorCode" => "ERROR",
 			"errorMessage" => $e->getMessage(),
-			"url" = "",
+			"url" => "",
 		);
 	}	
 }
